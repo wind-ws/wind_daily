c97607870e17e291c276da70a9269e3593218575
wind-ws <1424343223@qq.com>
2023-06-02 16:28:46 +0800

    这样应该是没问题的,吃饭后来测试

--- src-tauri/src/sqlite/table/config.rs
+++ src-tauri/src/sqlite/table/config.rs
@@ -1,6 +1,6 @@
 
 
-use diesel::{prelude::*, sqlite::Sqlite};
+use diesel::{prelude::*, select, dsl::exists};
 use serde::{Deserialize, Serialize};
 
 use crate::{sqlite::schema::config, other::user::user_db::Db, from_to_sql_json};
@@ -19,23 +19,91 @@ pub struct Config {
 }
 
 
-/// 为json结构实现,方便快速存取
-/// 专门服务 Config 表
-trait ConfigKey<ST>
+
+
+trait ConfigJson
 where
-    Self: serde::Serialize + 
-        serde::de::DeserializeOwned + 
-        diesel::Queryable<ST,Sqlite>+
-        'static{
-    fn get_key() -> &'static str;
+    Self:serde::Serialize+serde::de::DeserializeOwned{
+    fn get_key()-> &'static str;
     fn get_json()->Self{
         use crate::sqlite::schema::config::dsl::*;
         let mut db=Db.get_db().unwrap();
-        config
+        let s:String = config
             .filter(key.eq(Self::get_key()))
             .select(json)
-            .first::<Self>(&mut db).unwrap()
+            .first::<String>(&mut db).unwrap();
+        serde_json::from_str(&s).unwrap()
+    }
+    fn _insert_json(&self,db:&mut r2d2::PooledConnection<diesel::r2d2::ConnectionManager<SqliteConnection>>){
+        use crate::sqlite::schema::config::dsl::*;
+        diesel::insert_into(config)
+            .values(Config{
+                id:0,//自动增加,不用关心
+                key:Self::get_key().to_string(),
+                json:serde_json::to_string(self).unwrap()
+            })
+            .execute(db)
+            .unwrap();
+    }
+    fn _updata_json(&self,db:&mut r2d2::PooledConnection<diesel::r2d2::ConnectionManager<SqliteConnection>>){
+        use crate::sqlite::schema::config::dsl::*;
+        diesel::update(config.filter(json.eq(Self::get_key().to_string())))
+            .set(json.eq(serde_json::to_string(self).unwrap()))
+            .execute(db)
+            .unwrap();
+    }
+    /// 如果存在key,那么updata.不存在则 insert
+    fn set_json(&self){
+        use crate::sqlite::schema::config::dsl::*;
+        let mut db=Db.get_db().unwrap();
+        let a =select(exists(config.filter(key.eq(Self::get_key().to_string()))))
+            .get_result::<bool>(&mut db);
+        match a {
+            Ok(v) => {
+                if v {
+                    self._updata_json(&mut db);
+                }else {
+                    self._insert_json(&mut db);
+                }
+            },
+            Err(_) => panic!("谁会知道为什么这里会panic呢~"),
+        }
     }
 }
 
 
+pub enum ConfigKey {
+    Theme
+}
+impl ConfigKey {
+    pub fn get_key(&self) -> &'static str{
+        match self {
+            ConfigKey::Theme => "theme",
+        }
+    }
+    pub fn get_json<T>(&self)->T
+    where
+        T:serde::Serialize+serde::de::DeserializeOwned{
+        use crate::sqlite::schema::config::dsl::*;
+        let mut db=Db.get_db().unwrap();
+        let s:String = config
+            .filter(key.eq(self.get_key()))
+            .select(json)
+            .first::<String>(&mut db).unwrap();
+        serde_json::from_str::<T>(&s).unwrap()
+    }
+    pub fn set_json<T>(&self,v_json:T)
+    where
+        T:serde::Serialize+serde::de::DeserializeOwned{
+        use crate::sqlite::schema::config::dsl::*;
+        let mut db=Db.get_db().unwrap();
+        diesel::insert_into(config)
+            .values(Config{
+                id:0,//自动增加,不用关心
+                key:self.get_key().to_string(),
+                json:serde_json::to_string(&v_json).unwrap()
+            })
+            .execute(&mut db)
+            .unwrap();
+    }
+}
--- src-tauri/src/sqlite/table/config/theme.rs
+++ src-tauri/src/sqlite/table/config/theme.rs
@@ -1,6 +1,6 @@
 use diesel::{*, sql_types::Text};
 
-use crate::{from_to_sql_json, sqlite::schema::config};
+use crate::{from_to_sql_json, sqlite::schema::config, other::user::user_db::Db};
 
 
 
@@ -13,8 +13,38 @@ pub struct ThemeJson {
 }
 from_to_sql_json!(ThemeJson);
 
-impl super::ConfigKey<config::SqlType> for ThemeJson {
-    fn get_key() -> &'static str {
+impl super::ConfigJson for ThemeJson {
+    fn get_key()-> &'static str {
         "theme"
     }
 }
+
+// impl ThemeJson {
+//     fn get_key() -> &'static str {
+//         "theme"
+//     }
+//     fn get_json()->Self{
+//         use crate::sqlite::schema::config::dsl::*;
+//         let mut db=Db.get_db().unwrap();
+//         let s:String = config
+//             .filter(key.eq(Self::get_key()))
+//             .select(json)
+//             .first::<String>(&mut db).unwrap();
+//         serde_json::from_str(&s).unwrap()
+//     }
+//     pub fn set_json(&self,v_json:Self)
+//     where
+//         Self:serde::Serialize+serde::de::DeserializeOwned{
+//         use crate::sqlite::schema::config::dsl::*;
+//         let mut db=Db.get_db().unwrap();
+//         diesel::insert_into(config)
+//             .values(super::Config{
+//                 id:0,//自动增加,不用关心
+//                 key:Self::get_key().to_string(),
+//                 json:serde_json::to_string(&v_json).unwrap()
+//             })
+//             .execute(&mut db)
+//             .unwrap();
+//     }
+
+// }
